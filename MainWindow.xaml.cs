c5d23c12ebd88765f84aa7a2f4a9464ab2928480
Zack Shih <rezacks@gmail.com>
2013-05-26 00:11:48 +0800

    Initial realease.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,162 @@
+﻿/**
+ * National Taipei Univeristy of Technology
+ * Multimedia Information and Technology Integrated Lab
+ * Date: 2013/05/25
+ * Author: YuLun Shih
+ **/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using ImageManipulationExtensionMethods;
+
+using System.ComponentModel;
+using System.Windows.Threading;
+using System.Runtime.InteropServices;
+
+namespace ImageProcess
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public delegate void EventHandler(object sender, ImageSource e);
+
+        private BackgroundWorker updateCam;
+        private Capture webcam = null;
+        private bool trackStart = false;
+        private System.Drawing.Rectangle selectingWindow;
+        private ObjectTracking objTracking = null;
+        private bool isSelecting = false;
+        private Point startPos;
+        private Point endPos;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void init_updateCam()
+        {
+            webcam = new Capture(0);
+            updateCam = new BackgroundWorker();
+            updateCam.DoWork += updateCamWorker;
+            updateCam.RunWorkerAsync();
+            updateCam.RunWorkerCompleted += (object sender, RunWorkerCompletedEventArgs e) =>
+            {
+                updateCam.RunWorkerAsync();
+            };
+        }
+
+        private void updateCamWorker(object sender, DoWorkEventArgs e)
+        {
+            using (Image<Bgr, Byte> frame = webcam.QueryFrame().Flip(Emgu.CV.CvEnum.FLIP.HORIZONTAL))
+            {
+                if (trackStart)
+                {
+                    if (objTracking == null)
+                    {
+                        objTracking = new ObjectTracking(frame, selectingWindow);
+                    }
+                    else
+                    {
+                        System.Drawing.Rectangle result = objTracking.Tracking(frame);
+
+                        frame.Draw(result, new Bgr(0, 255, 0), 3);
+
+                        Dispatcher.Invoke(DispatcherPriority.Normal,
+                            new Action(
+                                delegate()
+                                {
+                                    if (objTracking != null)
+                                    {
+                                        inputImage.Source = frame.ToBitmapSource();
+                                        hueImage.Source = objTracking.hue.ToBitmapSource();
+                                        backprojectImage.Source = objTracking.backproject.ToBitmapSource();
+                                        maskImage.Source = objTracking.mask.ToBitmapSource();
+                                    }
+                                }
+                                ));
+                    }
+                }
+                else
+                {
+                    Dispatcher.Invoke(DispatcherPriority.Normal,
+                        new Action(
+                            delegate()
+                            {
+                                inputImage.Source = frame.ToBitmapSource();
+                            }
+                            ));
+                }
+            }
+        }
+
+        private void exitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void inputImage_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            isSelecting = true;
+            startPos = new Point(e.GetPosition(inputImage).X, e.GetPosition(inputImage).Y);
+            Console.WriteLine("DOWN!" + startPos);
+        }
+
+        private void inputImage_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            inputImageCanvas.Children.Clear();
+            isSelecting = false;
+            endPos = new Point(Math.Min(e.GetPosition(inputImage).X, inputImage.Width),
+                                Math.Min(e.GetPosition(inputImage).Y, inputImage.Height));
+            Console.WriteLine("UP!" + endPos);
+
+            int xRate = (int)(inputImage.Source.Width / inputImage.Width);
+            int yRate = (int)(inputImage.Source.Height / inputImage.Height);
+            selectingWindow = new System.Drawing.Rectangle(
+                (int)startPos.X * xRate, (int)startPos.Y * yRate,
+                (int)(endPos.X - startPos.X) * xRate, (int)(endPos.Y - startPos.Y) * yRate);
+
+            trackStart = true;
+            objTracking = null;
+        }
+
+        private void inputImage_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isSelecting)
+            {
+                Point movePos = new Point(e.GetPosition(inputImage).X, e.GetPosition(inputImage).Y);
+                inputImageCanvas.Children.Clear();
+                Rectangle rectangle = new Rectangle();
+                rectangle.SetValue(Canvas.LeftProperty, (double)startPos.X);//Math.Min(startPos.X, endPos.X)
+                rectangle.SetValue(Canvas.TopProperty, (double)startPos.Y);//Math.Min(startPos.Y, endPos.Y)
+                rectangle.Width = Math.Abs(movePos.X - startPos.X);
+                rectangle.Height = Math.Abs(movePos.Y - startPos.Y);
+                rectangle.Stroke = new SolidColorBrush() { Color = Colors.Red, Opacity = 0.75f };
+                rectangle.StrokeThickness = 3;
+                inputImageCanvas.Children.Add(rectangle);
+            }
+        }
+
+        private void startBtn_Click(object sender, RoutedEventArgs e)
+        {
+            init_updateCam();
+            startBtn.IsEnabled = false;
+        }
+    }
+}
---
+++ ObjectTracking.cs
@@ -0,0 +1,114 @@
+﻿/**
+ * National Taipei Univeristy of Technology
+ * Multimedia Information and Technology Integrated Lab
+ * Date: 2013/05/25
+ * Author: YuLun Shih
+ **/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Drawing;
+
+namespace ImageProcess
+{
+    class ObjectTracking
+    {
+        public Image<Hsv, Byte> hsv;
+        public Image<Gray, Byte> hue;
+        public Image<Gray, Byte> mask;
+        public Image<Gray, Byte> backproject;
+        public DenseHistogram hist;
+        private Rectangle trackingWindow;
+        private MCvConnectedComp trackcomp;
+        private MCvBox2D trackbox;
+
+        public ObjectTracking(Image<Bgr, Byte> image, Rectangle ROI)
+        {
+            // Initialize parameters
+            trackbox = new MCvBox2D();
+            trackcomp = new MCvConnectedComp();
+            hue = new Image<Gray, byte>(image.Width, image.Height);
+            hue._EqualizeHist();
+            mask = new Image<Gray, byte>(image.Width, image.Height);
+            hist = new DenseHistogram(30, new RangeF(0, 180));
+            backproject = new Image<Gray, byte>(image.Width, image.Height);
+
+            // Assign Object's ROI from source image.
+            trackingWindow = ROI;
+
+            // Producing Object's hist
+            CalObjectHist(image);
+        }
+
+        public Rectangle Tracking(Image<Bgr, Byte> image)
+        {
+            UpdateHue(image);
+
+            // Calucate BackProject
+            backproject = hist.BackProject(new Image<Gray, Byte>[] { hue });
+
+            // Apply mask
+            backproject._And(mask);
+
+            // Tracking windows empty means camshift lost bounding-box last time
+            // here we give camshift a new start window from 0,0 (you could change it)
+            if (trackingWindow.IsEmpty || trackingWindow.Width==0 || trackingWindow.Height==0)
+            {
+                trackingWindow = new Rectangle(0, 0, 100, 100);
+            }
+            CvInvoke.cvCamShift(backproject, trackingWindow,
+                new MCvTermCriteria(10, 1), out trackcomp, out trackbox);
+
+            // update tracking window
+            trackingWindow = trackcomp.rect;
+
+            return trackingWindow;
+        }
+
+        private void CalObjectHist(Image<Bgr, Byte> image)
+        {
+            UpdateHue(image);
+
+            // Set tracking object's ROI
+            hue.ROI = trackingWindow;
+            mask.ROI = trackingWindow;
+            hist.Calculate(new Image<Gray, Byte>[] { hue }, false, mask);
+
+            // Scale Historgram
+            float max=0, min=0, scale=0;
+            int[] minLocations, maxLocations;
+            hist.MinMax(out min, out max, out minLocations, out maxLocations);
+            if (max != 0)
+            {
+                scale = 255 / max;
+            }
+            CvInvoke.cvConvertScale(hist.MCvHistogram.bins, hist.MCvHistogram.bins, scale, 0);
+
+            // Clear ROI
+            hue.ROI = System.Drawing.Rectangle.Empty;
+            mask.ROI = System.Drawing.Rectangle.Empty;
+
+            // Now we have Object's Histogram, called hist.
+        }
+
+        private void UpdateHue(Image<Bgr, Byte> image)
+        {
+            // release previous image memory
+            if (hsv != null)    hsv.Dispose();
+            hsv = image.Convert<Hsv, Byte>();
+
+            // Drop low saturation pixels
+            mask = hsv.Split()[1].ThresholdBinary(new Gray(60), new Gray(255));
+            CvInvoke.cvInRangeS(hsv, new MCvScalar(0, 30, Math.Min(10, 255), 0),
+                new MCvScalar(180, 256, Math.Max(10, 255), 0), mask);
+
+            // Get Hue
+            hue = hsv.Split()[0];
+        }
+
+
+    }
+}
